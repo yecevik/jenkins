390580ffc494b40ca113ab2f4c21308c5c6bed65
yecevik <engcvk@gmail.com>
2023-03-22 20:37:48 -0400

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
 
+
+            System.out.println("Yusuf");
+
         }
-        System.out.println("Hello world!");
-        Thread.sleep(4000);
     }
 }
